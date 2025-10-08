aed3a5c45314fd5e13516c9cf49f52b46cde2fba
itstwoam <github@itstwoam.com>
2025-10-08 17:08:09 -0400

    Fixed test to check operations for real this time

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -15,7 +15,7 @@ func TestGetAPIKey(t *testing.T) {
 		"Valid Bearer token": {
 			header: http.Header{"Authorization": {"ApiKey abc123"}}, 
 			want: "abc123", 
-			expErr: true,
+			expErr: false,
 		},
 		"Missing Authorization": {
 			header: http.Header{},
