8f8a2c9cd15b10d9fc6500502dfa91f9c4596004
itstwoam <github@itstwoam.com>
2025-10-10 21:52:56 -0400

    Fixed formatting

--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"database/sql"
 	"embed"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"time"
-	"github.com/go-chi/chi"
-	"github.com/go-chi/cors"
-	"github.com/joho/godotenv"
 
 	"github.com/bootdotdev/learn-cicd-starter/internal/database"
 
@@ -85,11 +85,11 @@ func main() {
 
 	v1Router.Get("/healthz", handlerReadiness)
 	readHeaderTime, _ := time.ParseDuration("5s")
-	
+
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
 		ReadHeaderTimeout: readHeaderTime,
 	}
 
